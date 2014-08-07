8b2fc401eda093d160fb3a67216e816a82644c1e
tuannn <ntuan135@gmail.com>
2014-08-07 18:29:07 +0700

    fix bootstrap heroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Sign the user in and redirect to the user's show page.
-      redirect_to users_path
       sign_in user
+      redirect_to users_path
+      
     else
       flash[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
